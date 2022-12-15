8a73c3a6833c07cfb2cc0a0c848fe0e3fa77f059
David Gubitosi <david.gubitosi@gmail.com>
2022-12-15 17:07:17 -0500

    day 15 part 2 after refactoring, brute force in 5 minutes ;)

--- day15/solution.py
+++ day15/solution.py
@@ -6,14 +6,55 @@ sensors = dict()
 beacons_at_y = set()
 
 options = {
-    'test.txt': 10,
-    'input.txt': 2000000
+    'test.txt': [10, 20],
+    'input.txt': [2000000, 4000000]
 }
 
 name = 'input.txt'
-y = options[name]
-
+y = options[name][0]
+y_range = options[name][1]
 x_ranges = list()
+
+def x_range(position, distance, y, debug=False):
+    px, py = position
+    _range = []
+
+    # for y, solve for x
+    # d = abs(x-sx) + abs(y-sy)
+    # abs(x-sx) = dist - abs(y-sy)
+    x1 = px - (d - abs(y-py))
+    x2 = px + (d - abs(y-py))
+    xr = (min(x1, x2), max(x1, x2))
+
+    # check
+    d1 = dist(position, (xr[0],y))
+    d2 = dist(position, (xr[1],y))
+    if d1 <= distance and d2 <= distance:
+        if debug: print(f'sensor at {position}, d={distance}, y={y}, x-range={xr}')
+        _range = xr
+
+    return _range
+
+def squash(source, debug=False):
+    _ranges = sorted(source)
+    if debug: print(f'squash: {_ranges}')
+    if len(_ranges) > 1:
+        _ranges.sort()
+        i = 1
+        while i < len(_ranges):
+            a = _ranges[i-1]
+            b = _ranges[i]
+            if debug: print(f'range[{i-1}]={a}, range[{i}]={b}')
+            # overlaps or adjacent
+            if a[0] <= b[0] <= a[1]+1:
+                n = (a[0], max(a[1], b[1]))
+                if debug: print(f'.. reduced to {n}')
+                _ranges[i-1] = n
+                del _ranges[i]
+            else:
+                i += 1
+        return _ranges
+
 with open(name) as f:
     for line in f:
         line = line.strip()
@@ -33,48 +74,51 @@ with open(name) as f:
 
         # manhattan distance
         d = dist(s, b)
-        sensors.setdefault(s, dict())
-        sensors[s]['b'] = d
+        sensors[s] = d
 
         # collect the beacons at row y
         if by == y:
             beacons_at_y.add(b)
 
-        # for y, solve for x
-        # d = abs(x-sx) + abs(y-sy)
-        # abs(x-sx) = dist - abs(y-sy)
-        x1 = sx - (d - abs(y-sy))
-        x2 = sx + (d - abs(y-sy))
-        xr = (min(x1, x2), max(x1, x2))
+        _x_range = x_range(s, d, y, debug=True)
+        if _x_range:
+            x_ranges.append(_x_range)
 
-        # check sensors
-        dd1 = dist(s, (xr[0],y))
-        dd2 = dist(s, (xr[1],y))
-        if dd1 <= d and dd2 <= d:
-            print(f'sensor at {s}, d={d}, y={y}, x-range={xr}')
-            x_ranges.append(xr)
-
-            # list of x ranges per sensor
-            # that continually is reduced
-            if len(x_ranges) > 1:
-                x_ranges.sort()
-                i = 1
-                while i < len(x_ranges):
-                    print(x_ranges)
-                    a = x_ranges[i-1]
-                    b = x_ranges[i]
-                    print(f'x[{i-1}]={a}, x[{i}]={b}')
-                    # overlaps or adjacent
-                    if a[0] <= b[0] <= a[1]+1:
-                        n = (a[0], max(a[1], b[1]))
-                        print(f'.. reduced to {n}')
-                        x_ranges[i-1] = n
-                        del x_ranges[i]
-                    else:
-                        i += 1
-                print()
+x_ranges = squash(x_ranges, debug=True)
 
 not_present = 0
 for x in x_ranges:
     not_present += x[1] - x[0] + 1
 print("part1:", not_present - len(beacons_at_y))
+print()
+
+# part2
+# not bad, brute force takes 5 minutes
+print('part2 starting ...')
+
+import time
+st = time.time()
+
+for y in range(y_range):
+    x_ranges.clear()
+    if y and y % 100_000 == 0:
+        et = time.time() - st
+        print(f'{y} .. {et:.3f} s')
+
+    for s in sensors:
+        d = sensors[s]
+
+        _x_range = x_range(s, d, y)
+        if _x_range:
+            x_ranges.append(_x_range)
+
+    x_ranges = squash(x_ranges)
+    if len(x_ranges) == 2:
+        et = time.time() - st
+        print(f'{y} .. {et:.3f} s')
+        print(x_ranges, y)
+        x = x_ranges[0][1] + 1
+        if x != x_ranges[1][0] - 1:
+            raise Exception('Barf!')
+        print("part2:", x * 4000000 + y)
+        break
