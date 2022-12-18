7e5fcbc2799ad33c7cd9c69d4645400330b13d70
David Gubitosi <david.gubitosi@gmail.com>
2022-12-18 16:27:45 -0500

    day 18

--- day18/solution.py
+++ day18/solution.py
@@ -21,7 +21,7 @@ for i in range(len(cubes)):
         b = cubes[j]
         diff = abs(a[0] - b[0]) + abs(a[1] - b[1]) + abs(a[2] - b[2])
         if diff == 1:
-            print(f'adjacent: {i}:{cubes[i]} and {j}:{cubes[j]}')
+            #print(f'adjacent: {i}:{cubes[i]} and {j}:{cubes[j]}')
             adjacent[i].append(j)
 
 total = 0
@@ -42,28 +42,106 @@ print(_x)
 print(_y)
 print(_z)
 
+def _print():
+    icons = ['~', '.', '#']
+    for i, z in enumerate(planes):
+        print()
+        print("z-plane:", i)
+        row = '+'
+        for j in range(len(z[0])):
+            row += f'{str(j)[-1]}'
+        print(row)
+        for j, y in enumerate(z):
+            row = f'{str(j)[-1]}'
+            for x in y:
+                row += icons[x+1]
+            print(row)
+        print("z-plane:", i)
+
 planes = list()
-row = [0]*(_x[-1]+1)
+row = [-1]*(_x[-1]+1)
 for z in range(0, _z[-1]+1):
     planes.append([row[:] for i in range(_y[-1]+1)])
     for c in cubes:
         if z == c[2]:
             planes[c[2]][c[1]][c[0]] = 1
 
-for i, z in enumerate(planes):
-    print()
-    print("z-plane:", i)
-    row = '+'
-    for j in range(len(z[0])):
-        row += f'{str(j)[-1]}'
-    print(row)
-    for j, y in enumerate(z):
-        row = f'{str(j)[-1]}'
-        for x in y:
-            if x:
-                row += '#'
-            else:
-                row += '.'
-        print(row)
-    print("z-plane:", i)
+# z = 0 -> max
+for i in range(len(planes)):
+    for j in range(len(planes[z])):
+        for k in range(len(planes[z][y])):
+            if planes[i][j][k] < 0:
+                # outer plane
+                if i == 0:
+                    planes[i][j][k] = 0
+                # otherwise check the adjacent plane
+                else:
+                    if planes[i-1][j][k] == 0:
+                        planes[i][j][k] = 0
+
+# y = 0 -> max
+for i in range(len(planes)):
+    for j in range(len(planes[z])):
+        for k in range(len(planes[z][y])):
+            if planes[i][j][k] < 0:
+                # outer plane
+                if j == 0:
+                    planes[i][j][k] = 0
+                # otherwise check the adjacent plane
+                else:
+                    if planes[i][j-1][k] == 0:
+                        planes[i][j][k] = 0
+
+# x = 0 -> max
+for i in range(len(planes)):
+    for j in range(len(planes[z])):
+        for k in range(len(planes[z][y])):
+            if planes[i][j][k] < 0:
+                # outer plane
+                if k == 0:
+                    planes[i][j][k] = 0
+                # otherwise check the adjacent plane
+                else:
+                    if planes[i][j][k-1] == 0:
+                        planes[i][j][k] = 0
+
+# z = max -> 0
+for i in range(len(planes)-1, -1, -1):
+    for j in range(len(planes[z])):
+        for k in range(len(planes[z][y])):
+            if planes[i][j][k] < 0:
+                # outer plane
+                if i == len(planes)-1:
+                    planes[i][j][k] = 0
+                # otherwise check the plane below
+                else:
+                    if planes[i+1][j][k] == 0:
+                        planes[i][j][k] = 0
+
+# y = max -> 0
+for i in range(len(planes)):
+    for j in range(len(planes[z])-1, -1, -1):
+        for k in range(len(planes[z][y])):
+            if planes[i][j][k] < 0:
+                # top plane
+                if j == len(planes[z])-1:
+                    planes[i][j][k] = 0
+                # otherwise check the adjacent plane
+                else:
+                    if planes[i][j+1][k] == 0:
+                        planes[i][j][k] = 0
+# x = max -> 0
+for i in range(len(planes)):
+    for j in range(len(planes[z])):
+        for k in range(len(planes[z][y])-1, -1, -1):
+            if planes[i][j][k] < 0:
+                # otherwise check the adjacent plane
+                if k == len(planes[z][y])-1:
+                    planes[i][j][k] = 0
+                # otherwise check the plane below
+                else:
+                    if planes[i][j][k+1] == 0:
+                        planes[i][j][k] = 0
+
+_print()
 
