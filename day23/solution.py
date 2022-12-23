effc15ac9e2a090e2e3816ab925a98cc6d01fb0e
David Gubitosi <david.gubitosi@gmail.com>
2022-12-23 11:26:27 -0500

    day 23 part 1 solved

--- day23/solution.py
+++ day23/solution.py
@@ -1,5 +1,5 @@
 
-filename = "test.txt"
+filename = "input.txt"
 elves = dict()
 with open(filename) as f:
     y = 0
@@ -30,65 +30,92 @@ def _print(text='', extra=2):
                 row += '.'
         print(row)
 
-
 def move(e, debug=False):
     y, x = e
-    np = None
+    pos = None
 
-    # directions
-    DIRECTIONS = {
-        "north": [(y-1,x-1), (y-1,x), (y-1,x+1)], # n = y-1
-        "south": [(y+1,x-1), (y+1,x), (y+1,x+1)], # s = y+1
-        "west":  [(y-1,x-1), (y,x-1), (y+1,x-1)], # w = x-1
-        "east":  [(y-1,x+1), (y,x+1), (y+1,x+1)], # e = x+1
+    ADJACENT = {
+        'NW': (y-1,x-1),
+        'N':  (y-1,x),
+        'NE': (y-1,x+1),
+        'E':  (y,x+1),
+        'SE': (y+1,x+1),
+        'S':  (y+1,x),
+        'SW': (y+1,x-1),
+        'W':  (y,x-1),
     }
-    _directions = ["north", "south", "west", "east"]
+    _adjacent = list()
+    for p in ADJACENT.values():
+        if p in elves:
+            _adjacent.append(p)
 
-    # pick a direction
-    len_d = len(_directions)
-    d = i % len_d
-    np = None
-    for _ in range(len_d):
-        if np: break
-        if d >= len_d:
-            d -= len_d
+    if debug: print('  Adjacent:', _adjacent)
+    if _adjacent:
+        # directions
+        DIRECTIONS = {
+            "north": [ADJACENT['NW'], ADJACENT['N'], ADJACENT['NE']],
+            "south": [ADJACENT['SW'], ADJACENT['S'], ADJACENT['SE']],
+            "west":  [ADJACENT['NW'], ADJACENT['W'], ADJACENT['SW']],
+            "east":  [ADJACENT['NE'], ADJACENT['E'], ADJACENT['SE']],
+        }
+        _directions = ["north", "south", "west", "east"]
 
-        _direction = _directions[d]
-        if debug: print(f'Testing {_direction}:')
-        valid = list()
-        for p in DIRECTIONS[_direction]:
-            if p in elves:
-                continue
-            else:
-                valid.append(p)
-        if debug: print(valid)
-        if len(valid) == 3:
-            np = valid[1]
-            break
-        d += 1
+        # pick a direction
+        len_d = len(_directions)
+        d = i % len_d
+        for _ in range(len_d):
+            if pos: break
+            if d >= len_d:
+                d -= len_d
 
-    return np
+            _direction = _directions[d]
+            _valid = list()
+            for p in DIRECTIONS[_direction]:
+                if p in _adjacent:
+                    break
+                else:
+                    _valid.append(p)
+            if debug: print(f'  {_direction.title()}', _valid)
+            if len(_valid) == 3:
+                if debug: print(' ', _occupied)
+                pos = _valid[1]
+                break
+            d += 1
 
-debug = True
-_print(text='== Initial State ==')
+    return pos
+
+debug = False
+if debug: _print(text='== Initial State ==')
 i = 0
-while i < 2:
+while i < 10:
     _elves = dict()
+    if debug: print(f'\nRound {i+1}, first half')
     for e in elves:
         if debug: print(f'Elf {e}')
         np = move(e, debug)
         if np:
             _elves.setdefault(np, list()).append(e)
 
+    if debug: print(f'\nRound {i+1}, second half')
     for e in _elves:
         if len(_elves[e]) == 1:
             st = _elves[e][0]
-            if debug: print(f'Move {st} to {e}')
+            if debug: print(f'  Move: {st} -> {e}')
             del elves[st]
             elves[e] = True
 
     i += 1
-    if i in [1,2,3,4,5,10]:
-        print()
-        _print(text=f'== End of Round {i} ==')
+    if debug:
+        if i in [1,2,3,4,5,10]:
+            print()
+            _print(text=f'== End of Round {i} ==')
 
+# unoccupied tiles in minimum rectangle
+e = len(elves)
+_y, _x = map(list, zip(*elves))
+_y.sort()
+_x.sort()
+height = _y[-1] - _y[0] + 1
+width = _x[-1] - _x[0] + 1
+area = height * width
+print("part1: ", area - e)
